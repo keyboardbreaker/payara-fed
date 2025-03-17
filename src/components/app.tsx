229bba893abc2cdd9699a3a0e25990a764ae9d39
yujin_tan <yujinost@gmail.com>
2025-03-17 18:00:28 +0000

    toggle theme using useState

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "./banner";
+import { Theme } from "@/enums/themes";
 
 const App = () => {
-    return <Banner />
+    const [theme, setTheme] = useState<Theme>(Theme.light);
+
+    const toggleTheme = (): void => {
+        setTheme(theme === Theme.light ? Theme.dark : Theme.light);
+    };
+
+    return (
+        <>
+            <section className={`theme-${theme}`}>
+                <button onClick={toggleTheme}>{"Toggle Theme"}</button>
+                <Banner />
+            </section>
+        </>
+    )
 };
 
 export default App;
--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,7 +1,7 @@
 const Banner = () => {
     return (
         <header className="our-plan">
-            <h1 className="our-plan-title">Powerful features for powerful creators</h1>
+            <h1 className="our-plan-title">Powerful features for <span className="our-plan-title-highlight">powerful creators</span></h1>
             <p className="our-plan-subtitle">Choose a plan that&apos;s right for you</p>
         </header>
     );
