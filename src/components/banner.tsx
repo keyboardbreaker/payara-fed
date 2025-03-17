b73e4da1fe8d7c69fe01d5585af532ad2540ca26
yujin_tan <yujinost@gmail.com>
2025-03-17 16:08:52 +0000

    banner heading

---
+++ src/components/banner.tsx
@@ -0,0 +1,10 @@
+const Banner = () => {
+    return (
+        <header className="our-plan">
+            <h1 className="our-plan-title">Powerful features for powerful creators</h1>
+            <p className="our-plan-subtitle">Choose a plan that&apos;s right for you</p>
+        </header>
+    );
+};
+
+export default Banner;
